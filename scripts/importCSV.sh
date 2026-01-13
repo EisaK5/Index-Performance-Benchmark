2812548ca07910f82bf0ee395f1d6396a8ac47a8
EisaK5 <eisa@eisakazim.com>
2026-01-12 23:56:18 -0600

    prototype of script to import files

--- scripts/importCSV.sh
+++ scripts/importCSV.sh
@@ -1,1 +1,16 @@
-for
+
+psql -U postgres -d stockDB -c "CREATE TABLE stock_staging(date DATE, open FLOAT, high FLOAT, low FLOAT, close FLOAT, volume BIGINT, openint FLOAT);"
+for file in "$DIRECTORY"/*.txt
+do
+    filename="$(basename "$file")"
+    echo "importing $filename"
+
+    #truncate, load file, move data 
+    psql "" <<SQL BEGIN;
+    TRUNCATE stock_staging;
+    \copy stock_staging(date, open, high, low, close, volume, openint) FROM '$file' WITH (FORMAT csv, HEADER true);
+    INSERT INTO stocks (source_file, date, open, high, low, close, volume, openint) 
+    SELECT ('$filename', date, open, high, low, close, volume, openint) FROM stock_staging;
+    COMMIT;
+    SQL
+done
