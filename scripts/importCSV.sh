f62bcad9a71f742fd08c1437f210b069584ac0d3
EisaK5 <eisa@eisakazim.com>
2026-01-26 16:32:53 -0600

    final script

--- scripts/importCSV.sh
+++ scripts/importCSV.sh
@@ -1,16 +1,40 @@
+DIRECTORY=
+DB_URL=""
+PGPASSWORD=
+CREATE TABLE IF NOT EXISTS stock_staging(
+  date DATE,
+  open DOUBLE PRECISION,
+  high DOUBLE PRECISION,
+  low DOUBLE PRECISION,
+  close DOUBLE PRECISION,
+  volume BIGINT,
+  openint DOUBLE PRECISION
+);
 
-psql -U postgres -d stockDB -c "CREATE TABLE stock_staging(date DATE, open FLOAT, high FLOAT, low FLOAT, close FLOAT, volume BIGINT, openint FLOAT);"
+CREATE TABLE IF NOT EXISTS stocks(
+  id SERIAL PRIMARY KEY,
+  source_file VARCHAR(255),
+  date DATE,
+  open DOUBLE PRECISION,
+  high DOUBLE PRECISION,
+  low DOUBLE PRECISION,
+  close DOUBLE PRECISION,
+  volume BIGINT,
+  openint DOUBLE PRECISION
+);"
+
 for file in "$DIRECTORY"/*.txt
 do
     filename="$(basename "$file")"
     echo "importing $filename"
 
-    #truncate, load file, move data 
-    psql "" <<SQL BEGIN;
-    TRUNCATE stock_staging;
-    \copy stock_staging(date, open, high, low, close, volume, openint) FROM '$file' WITH (FORMAT csv, HEADER true);
-    INSERT INTO stocks (source_file, date, open, high, low, close, volume, openint) 
-    SELECT ('$filename', date, open, high, low, close, volume, openint) FROM stock_staging;
-    COMMIT;
-    SQL
+    PG
+BEGIN;
+TRUNCATE stock_staging;
+\copy stock_staging(date, open, high, low, close, volume, openint) FROM '$file' WITH (FORMAT csv, HEADER true, DELIMITER ',')
+INSERT INTO stocks (source_file, date, open, high, low, close, volume, openint) 
+SELECT '$filename', date, open, high, low, close, volume, openint 
+FROM stock_staging;
+COMMIT;
+SQL
 done
